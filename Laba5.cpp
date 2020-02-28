84284aadb28230c17ae11b1190a47080ad904480
Kiruchi <60315531+K1ruchi@users.noreply.github.com>
2020-02-28 20:11:37 +0300

    Add files via upload

---
+++ Laba5.cpp
@@ -0,0 +1,56 @@
+﻿//Задать матрицу размером NxM. Определить количество различных элементов матрицы (т.е. повторяющиеся элементы считать один раз).
+#include <iomanip> 
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+
+	int m, n, k, l, i, j, t, q;
+	char** a;
+	cout << "Vvedite razmer massiva n ";
+	cin >> n;
+	cout << "Vvedite razmer massiva m ";
+	cin >> m;
+	a = new char* [n];
+	for (i = 0; i < n; i++)
+		a[i] = new char[m];
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < m; j++)
+		{
+			cout << "Vvedite a[" << i << "][" << j << "] ";
+			cin >> a[i][j];
+		}
+	}
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < m; j++)
+		{
+			cout << setw(5) << a[i][j];
+		}
+		cout << endl;
+	}
+	t = 0;
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < m; j++)
+		{
+			q = true;
+			for (k = 0; k < (i + 1); k++)
+			{
+				for (l = 0; l < (j + 1); l++)
+				{
+					if (a[i][j] == a[k][l])
+					{
+						q = false;
+						break;
+					}
+				}
+				if (q) t++;
+			}
+		}
+	}
+	cout << "Kolichestvo razlichnih elementov " << t << endl;
+	return 0;
+}
